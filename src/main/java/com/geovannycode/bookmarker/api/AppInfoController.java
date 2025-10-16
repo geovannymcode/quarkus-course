a89860d9fd8155859d225c36dba98fa171f2e643
Geovanny Mendoza <me@geovannycode.com>
2025-10-16 12:00:00 -0500

    Commit retroactivo

---
+++ src/main/java/com/geovannycode/bookmarker/api/AppInfoController.java
@@ -0,0 +1,20 @@
+package com.geovannycode.bookmarker.api;
+
+import io.quarkus.logging.Log;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.time.Instant;
+
+@Path("/")
+public class AppInfoController {
+
+    public record AppInfo(String name, String version, String status) { }
+
+    @GET
+    @Path("")
+    public AppInfo getAppInfo() {
+        Log.infof("Getting app info at %s", Instant.now().toString());
+        return new AppInfo("Bookmarker", "1.0.0", "UP");
+    }
+}
