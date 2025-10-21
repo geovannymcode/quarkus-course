6ee4a2f8053ba8ae22d04d4c8bedb494a857b860
Geovanny Mendoza <me@geovannycode.com>
2025-10-21 12:00:00 -0500

    Commit retroactivo

---
+++ src/main/java/com/geovannycode/bookmarker/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.geovannycode.bookmarker.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
