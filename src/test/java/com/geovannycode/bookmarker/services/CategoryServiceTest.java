388427c19ea1e27f203899831f67fac27555610a
Geovanny Mendoza <me@geovannycode.com>
2025-10-23 12:00:00 -0500

    Commit retroactivo

--- src/test/java/com/geovannycode/bookmarker/services/CategoryServiceTest.java
+++ src/test/java/com/geovannycode/bookmarker/services/CategoryServiceTest.java
@@ -1,4 +1,33 @@
 package com.geovannycode.bookmarker.services;
 
-public class CategoryServiceTest {
+import com.geovannycode.bookmarker.entities.Category;
+import com.geovannycode.bookmarker.models.PagedResult;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+class CategoryServiceTest {
+    @Inject
+    CategoryService categoryService;
+
+    @Test
+    void testFindBySlug() {
+        Optional<Category> category = categoryService.findBySlug("java");
+        assertTrue(category.isPresent());
+        assertEquals("java", category.get().getSlug());
+    }
+
+    @Test
+    void testFindByPage() {
+        PagedResult<Category> pagedResult = categoryService.findByPage(1);
+        assertFalse(pagedResult.data().isEmpty());
+    }
+
 }
